4d8291a922d0513a7375d0d41562461aa93d579f
Joshua Levine <joshuaalevine3@gmail.com>
2023-05-21 00:42:31 -0400

    fixed build errors

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -8,10 +8,10 @@ export default function AboutPage() {
         Contact Information
       </h1>
       <p className='mb-3 text-lg'>
-        I'm currently looking for new opportunities. If you're interested in reaching out to me for work, to collaborate
-        on a project, or simply to connect please feel free to click on one of the links listed below! The best way to
-        reach me is via email or LinkedIn. I do not post much on social media, but I do promise that I am actively
-        engaged with anyone who reaches out.
+        I&apos;m currently looking for new opportunities. If you&apos;re interested in reaching out to me for work, to
+        collaborate on a project, or simply to connect please feel free to click on one of the links listed below! The
+        best way to reach me is via email or LinkedIn. I do not post much on social media, but I do promise that I am
+        actively engaged with anyone who reaches out.
       </p>
       <div className='flex flex-col'>
         <FancyLink href='https://www.linkedin.com/in/josh-lev/'>
